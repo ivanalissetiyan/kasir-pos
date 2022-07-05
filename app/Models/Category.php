c8221c512e3e22856c26f2770bc2979df1c8a299
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-07-06 02:17:13 +0700

    migration done

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
@@ -17,4 +18,28 @@ class Category extends Model
     protected $fillable = [
         'image', 'name', 'description'
     ];
+
+    /**
+     * products
+     *
+     * @return void
+     */
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
 }
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //create user
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'Admin@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        // Get all Permissions
+        $permissions = Permission::all();
+
+        // Get role Admin
+        $role = Role::find(1);
+
+        // Assign Permission to role
+        $role->syncPermissions($permissions);
+
+        //Assign Role to User
+        $user->assignRole($role);
+    }
+}
