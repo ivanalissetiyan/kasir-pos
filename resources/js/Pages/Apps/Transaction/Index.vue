81394ac705d23bf53df76e502c6c0c86c6ec1d6f
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-08-24 02:34:14 +0700

    menampilkan grand total transaction

--- resources/js/Pages/Apps/Transaction/Index.vue
+++ resources/js/Pages/Apps/Transaction/Index.vue
@@ -1,5 +1,4 @@
 <template>
-
     <Head>
         <title>Transaction - Aplikasi Kasir</title>
     </Head>
@@ -12,26 +11,39 @@
                             <div class="card-body">
                                 <div class="input-group mb-3">
                                     <span class="input-group-text"><i class="fa fa-barcode"></i></span>
-                                    <input type="text" class="form-control" placeholder="scan or input barcode">
+                                    <input type="text" class="form-control" v-model="barcode"
+                                        placeholder="scan or input barcode" @keyup="searchProduct">
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label fw-bold">Product Name</label>
-                                    <input type="text" class="form-control" placeholder="product name" readonly>
+                                    <input type="text" class="form-control" :value="product.title"
+                                        placeholder="product name" readonly>
                                 </div>
                                 <div class="mb-3">
                                     <label class="form-label fw-bold">Qty</label>
-                                    <input type="number" class="form-control text-center" placeholder="Qty" min="1">
+                                    <input type="number" class="form-control text-center" v-model="qty" min="1">
                                 </div>
                                 <div class="text-end">
-                                    <button
-                                        class="btn btn-warning btn-md border-0 shadow text-uppercase mt-3 me-2">Clear</button>
-                                    <button class="btn btn-primary btn-md border-0 shadow text-uppercase mt-3 me-2">Add
-                                        Item</button>
+                                    <button @click.prevent="clearSearch"
+                                        class="btn btn-warning btn-md border-0 shadow text-uppercase mt-3 me-2"
+                                        :disabled="!product.id">Clear</button>
+                                    <button @click.prevent="addToCart"
+                                        class="btn btn-primary btn-md border-0 shadow text-uppercase mt-3"
+                                        :disabled="!product.id">Add Item</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-8">
+
+                        <div v-if="session.error" class="alert alert-danger">
+                            {{ session.error }}
+                        </div>
+
+                        <div v-if="session.success" class="alert alert-success">
+                            {{ session.success }}
+                        </div>
+
                         <div class="card border-0 rounded-3 shadow border-top-success">
                             <div class="card-body">
                                 <div class="row">
@@ -39,7 +51,7 @@
                                         <h4 class="fw-bold">Grand Total</h4>
                                     </div>
                                     <div class="col-md-8 col-8 text-end">
-                                        <h4 class="fw-bold">Rp. 0</h4>
+                                        <h4 class="fw-bold">Rp. {{ formatPrice(grandTotal) }}</h4>
                                     </div>
                                 </div>
                             </div>
@@ -48,20 +60,21 @@
                             <div class="card-body">
                                 <div class="row mb-3">
                                     <div class="col-md-6">
-                                        <Label class="fw-bold">Cashier</Label>
+                                        <label class="fw-bold">Cashier</label>
                                         <!-- Value auth.user.name itu sama aja halnya seperti auth()->usser() / Auth::user() -->
                                         <input class="form-control" type="text" :value="auth.user.name" readonly>
                                     </div>
                                     <div class="col-md-6 float-end">
                                         <label class="fw-bold">Customer</label>
-                                        <VueMultiselect v-model="customer_id" label="name" track-by="name" :options="customers"></VueMultiselect>
+                                        <VueMultiselect v-model="customer_id" label="name" track-by="name"
+                                            :options="customers"></VueMultiselect>
                                     </div>
                                 </div>
                                 <hr>
                                 <table class="table table-bordered">
                                     <thead>
                                         <tr style="background-color: #e6e6e7;">
-                                            <th scope="col">#</th>
+                                            <th scope="col">No</th>
                                             <th scope="col">Nama Produk</th>
                                             <th scope="col">Harga</th>
                                             <th scope="col">Qty</th>
@@ -91,7 +104,6 @@
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -110,6 +122,13 @@ import { Head } from '@inertiajs/inertia-vue3';
 import VueMultiselect from 'vue-multiselect';
 import 'vue-multiselect/dist/vue-multiselect.css';
 
+// import ref from vue
+import { ref } from 'vue';
+
+// import axios
+import axios from 'axios';
+import { Inertia } from '@inertiajs/inertia';
+
 export default {
     //layout
     layout: LayoutApp,
@@ -123,7 +142,87 @@ export default {
     // Props
     props: {
         auth: Object,
-        customers: Array
+        customers: Array,
+        carts_total: Number,
+        session: Object
+    },
+
+    // Compotition API
+    setup(props) {
+
+        // Define state
+        const barcode = ref('');
+        const product = ref({});
+        const qty = ref(1);
+
+        // Method search product
+        const searchProduct = () => {
+
+            // Fetch with axios
+            axios.post('/apps/transactions/searchProduct', {
+
+                // Send Data Barcode
+                barcode: barcode.value
+
+            }).then(response => {
+                if (response.data.success) {
+                    // Assign response to state "product"
+                    product.value = response.data.data;
+                } else {
+                    // Set data "product" to empty object
+                    product.value = {};
+                }
+            });
+        }
+
+        // Method "Clear Search"
+        const clearSearch = () => {
+
+            // Set state "product" to empty object
+            product.value = {};
+
+            // Set state "barcode" to empty string
+            barcode.value = '';
+
+            // Set State "qty" to empty number qty
+            qty.value = (1);
+        }
+
+        // Define State Grand Total
+        const grandTotal = ref(props.carts_total);
+
+        // Method addToCart
+        const addToCart = () => {
+            // Send data to server
+            Inertia.post('/apps/transactions/addToCart', {
+
+                // Data
+                product_id: product.value.id,
+                qty: qty.value,
+                sell_price: product.value.sell_price,
+            }, {
+                onSuccess: () => {
+                    // Call Method "clearSearch"
+                    clearSearch();
+
+                    // Set qty to "1"
+                    qty.value = 1;
+
+                    // Update state "grand total"
+                    grandTotal.value = props.carts_total;
+                },
+            });
+        }
+
+        return {
+            barcode,
+            product,
+            searchProduct,
+            clearSearch,
+            qty,
+            grandTotal,
+            addToCart
+        }
     }
 }
 </script>
